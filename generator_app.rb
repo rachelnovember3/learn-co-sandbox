84a9d04daddf3b5665a3f3fb93f109d0eb23bfc8
rachelnovember3 <rachelnovember3@gmail.com>
2018-08-15 21:05:57 +0000

    automatically backed up by learn

--- generator_app.rb
+++ generator_app.rb
@@ -1,14 +1,14 @@
 # puts "What state are you interested in?"
 # state = gets.chomp
 # def rep_generator(state)
-#   if state == Alabama
-#     puts "Senators: Richard Shelby(R) and Doug Jones(D)"
-#     puts "House of Representatives(7): Bradley Byrne(R), Martha Roby(R), Mike Rogers(R), Robert Aderholt(R), Mo Brooks(R), Gary Palmer(R), and Terri Sewell(D)"
-  # elsif state == "Alaska"
-  #   puts "Senators: Lisa Murkowski(R) and Dan Sullivan(R)"
-  #   puts "House of Representatives(1): Don Young(R)"
-#   end
-# end
+  if state == Alabama
+    puts "Senators: Richard Shelby(R) and Doug Jones(D)"
+    puts "House of Representatives(7): Bradley Byrne(R), Martha Roby(R), Mike Rogers(R), Robert Aderholt(R), Mo Brooks(R), Gary Palmer(R), and Terri Sewell(D)"
+  elsif state == "Alaska"
+    puts "Senators: Lisa Murkowski(R) and Dan Sullivan(R)"
+    puts "House of Representatives(1): Don Young(R)"
+  end
+end
 
 # def complex_curfew_checker(time)
 #   # code goes here
